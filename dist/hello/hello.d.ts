d99882f80fd5e24bb1bdf3394ed42c4983b76938
ibrahim ÇOBANİ <ibrahim@imaconsult.com>
2022-05-17 17:13:24 +0300

    Models

--- dist/hello/hello.d.ts
+++
@@ -1,2 +0,0 @@
-declare const sayHello: () => string;
-export { sayHello };
--- dist/hello/hello.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sayHello = void 0;
-const sayHello = () => {
-    return "Hello world";
-};
-exports.sayHello = sayHello;
-//# sourceMappingURL=hello.js.map
